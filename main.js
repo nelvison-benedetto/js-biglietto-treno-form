f519b377ceb8fc185b6380bfe36822c437d2d696
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-10-09 17:05:23 +0200

    first done

---
+++ main.js
@@ -0,0 +1,12 @@
+const btn_calc = document.querySelector("div button");
+const price = 0.21;
+
+var miles = prompt("Please insert the miles: ");
+var age = prompt("Please insert the age: ");
+const calc = (miles,age) => {
+    return age > 65? (miles*price)*0.60 : ( age>18? miles*age : (miles*age)*0.20 );
+};
+
+btn_calc.addEventListener('click', function(){console.log(calc(miles,age)); });
+
+
