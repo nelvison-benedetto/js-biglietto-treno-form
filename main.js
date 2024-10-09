5c43a6b5b236c4a80cfc59e952a4c88ae026a0d6
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-10-09 18:09:05 +0200

    logic working

--- main.js
+++ main.js
@@ -11,14 +11,18 @@
 // btn_calc.addEventListener('click', function(){console.log(calc(miles,age)); });
 
 //MILESTONE 2
-const btn_submit = document.querySelector("div button");
+//const btn_submit = document.querySelector("div button");
 const price = 0.21;
 
 document.querySelector(".container form button").addEventListener("click",function(){
     //event.preventDefault();
     const firstname = document.getElementById("input_firstname").value;
     const lastname = document.getElementById("input_lastname").value;
+    const age = document.getElementById("input_age").value;
+    const miles = document.getElementById("input_miles").value;
 
-    document.getElementById("output").textContent = `${firstname} ${lastname}`;
+    const cost = (age,miles) => age > 65? (miles*price)*0.60 : ( age>18? miles*age : (miles*age)*0.20 );
+
+    document.getElementById("output").textContent = `${cost(age,miles)}`;
 });
 
